0876485092c860b7ec420092251763be54d36424
Ivan Echevarria <ivan@echevarria.io>
2021-09-27 11:42:47 -0700

    add binary conversion

--- 3-introduction-to-assembly-programming/binary_convert.asm
+++ 3-introduction-to-assembly-programming/binary_convert.asm
@@ -1,4 +1,14 @@
 section .text
 global binary_convert
 binary_convert:
-	
+	cmp byte [rdi], 0
+	je exit
+	shl rax, 1
+	cmp byte [rdi], 48
+	je next
+	add rax, 1
+next:
+	inc rdi
+	jmp binary_convert
+exit:
+	ret
