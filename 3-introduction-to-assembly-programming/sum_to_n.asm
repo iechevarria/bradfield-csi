7b39a152930e9a0db642f31dafab3875fe752a19
Ivan Echevarria <ivan@echevarria.io>
2021-09-26 23:06:00 -0700

    golf better

--- 3-introduction-to-assembly-programming/sum_to_n.asm
+++ 3-introduction-to-assembly-programming/sum_to_n.asm
@@ -1,10 +1,7 @@
 section .text
 global sum_to_n
 sum_to_n:
-	xor rax, rax
-.L1:
 	add rax, rdi
 	dec rdi
-	cmp rdi, 0		; redundant but just checking if behavior changes
-	jg .L1			; jnz doesn't work here - infinite loop
+	jg  sum_to_n
 	ret
