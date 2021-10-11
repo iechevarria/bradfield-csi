4a76e0f7a6c7634f4a77dd7793bcf07dd03854a5
Ivan Echevarria <ivan@echevarria.io>
2021-10-10 20:19:45 -0700

    add a start to monkey shell

---
+++ 8-exceptional-control-flow/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+func main () {
+	var sb strings.Builder
+	in := bufio.NewReader(os.Stdin)
+	fmt.Print("🐵 ")
+
+	for {
+		r, _, err := in.ReadRune()
+		if err == io.EOF {
+			fmt.Println("\n🙉🙈🙊")
+			break
+		} else if r == '\n' {
+			line := sb.String()
+
+			// try to fork + exec
+			args := strings.Fields(line)
+			cmd := exec.Command(args[0], args[1:]...)
+
+			// hook up command stdout to the shell's and run
+			cmd.Stdout = os.Stdout
+			err := cmd.Run()
+			if err != nil {
+				fmt.Println("🐒💩 " + args[0] + ": command not found")
+			}
+
+			// fmt.Println(line)
+
+			// reset terminal
+			fmt.Print("🐵 ")
+			sb.Reset()
+
+		} else {
+			sb.WriteRune(r)
+		}	
+	}
+}
