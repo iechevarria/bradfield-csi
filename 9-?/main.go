11fff497fca7a6692426470d3b7d9d8bc0dba803
Ivan Echevarria <ivan@echevarria.io>
2021-10-25 21:52:35 -0700

    add solution

--- 9-?/main.go
+++ 9-?/main.go
@@ -5,17 +5,27 @@ import (
 	"unsafe"
 )
 
+type Point struct { 
+	x int
+	y int
+}
+
 func SpecialLen(s string) int {
 	// Given a string, return its length without using len
 	p := unsafe.Pointer(uintptr(unsafe.Pointer(&s)) + unsafe.Sizeof(1))
 	return *(*int)(p)
 }
 
-func SpecialPoint(p point) int {
-	// TODO
+func SpecialPoint(p Point) int {
+	// Given a Point struct, return its y coordinate without using p.y
+	p_ := unsafe.Pointer(uintptr(unsafe.Pointer(&p)) + unsafe.Sizeof(1))
+	return *(*int)(p_)
 }
 
 func main () {
 	x := "HELLO WORLD"
 	fmt.Println(SpecialLen(x))
+
+	y := Point{55, 77}
+	fmt.Println(SpecialPoint(y))
 }
