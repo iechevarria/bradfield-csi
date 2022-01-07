461ca69beb8cfb84ea0cb447a482ad95bce841d2
Ivan Echevarria <ivan@echevarria.io>
2022-01-07 09:54:18 -0500

    get some response :)

--- 18-dns-client/main.go
+++ 18-dns-client/main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"bufio"
-    "bytes"
-    "encoding/binary"
+	"bytes"
+	"encoding/binary"
 	"fmt"
 	"net"
 )
@@ -43,39 +43,63 @@ type DnsHeader struct {
 func main() {
 	//    4    d    o    c    s    6    g    o    o    g   l     e    3    c   o    m   end
 	// 0x04 0x64 0x6f 0x63 0x73 0x06 0x67 0x6f 0x6f 0x67 0x6c 0x65 0x03 0x63 0x6f 0x6d 0x00
-	query := [...]byte{0x04, 0x64, 0x6f, 0x63, 0x73, 0x06, 0x67, 0x6f, 0x6f, 0x67, 0x6c, 0x65, 0x03, 0x63, 0x6f, 0x6d, 0x00}
+	request_qname := [...]byte{0x04, 0x64, 0x6f, 0x63, 0x73, 0x06, 0x67, 0x6f, 0x6f, 0x67, 0x6c, 0x65, 0x03, 0x63, 0x6f, 0x6d, 0x00}
 
-	dns_query := struct{
-        Header     DnsHeader
-        Query      [len(query)]byte
-        RecordType uint16
-        Class      uint16    
-    }{
-        DnsHeader{
-            12345, // id
-            0x0100, // flags
-            1,      // qd count
-            0,      // an count
-            0,      // ns count
-            0,      // ar count    
-        },
-		query,  // query
-		1,      // record type
-		1,      // class
+	dns_query := struct {
+		Header DnsHeader
+		QName  [len(request_qname)]byte
+		QType  uint16
+		QClass uint16
+	}{
+		DnsHeader{
+			12345,  // id
+			0x0100, // flags
+			1,      // qd count
+			0,      // an count
+			0,      // ns count
+			0,      // ar count
+		},
+		request_qname, // qname
+		1,             // qtype
+		1,             // qclass
 	}
 
+	// send request
 	conn, err := net.Dial("udp", "8.8.8.8:53")
 	check(err)
-    binary.Write(conn, binary.BigEndian, dns_query)
+	binary.Write(conn, binary.BigEndian, dns_query)
 
+	// get response
 	response := make([]byte, 2048)
-    _, err = bufio.NewReader(conn).Read(response)
+	_, err = bufio.NewReader(conn).Read(response)
 	check(err)
 
-    response_reader := bytes.NewReader(response)
-    response_header := DnsHeader{}
-    binary.Read(response_reader, binary.BigEndian, &response_header)
-    fmt.Println(response_header)
+	response_reader := bytes.NewReader(response)
+	response_header := DnsHeader{}
+	response_question := struct {
+		QName  [len(request_qname)]byte
+		QType  uint16
+		QClass uint16
+	}{}
+	binary.Read(response_reader, binary.BigEndian, &response_header)
+	binary.Read(response_reader, binary.BigEndian, &response_question)
 
+	if response_header.Id != 12345 {
+		panic("Wrong response id")
+	}
+	if response_header.QdCount != 1 {
+		panic("Wrong response number of queries")
+	}
+	if response_question.QName != request_qname {
+		panic("Wrong response qname")
+	}
+	if response_question.QType != 1 {
+		panic("Wrong response qtype")
+	}
+	if response_question.QClass != 1 {
+		panic("Wrong response qclass")
+	}
+
+	fmt.Println(response_question)
 	conn.Close()
 }
